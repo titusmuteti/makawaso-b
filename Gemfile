82e16b269f7d2c6398f529629d77f0ec8528db2a
Titus Mumo <titusmumo@gmail.com>
2023-06-08 19:16:36 +0300

    adding seed data

--- Gemfile
+++ Gemfile
@@ -70,3 +70,17 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
+gem "bcrypt", "~> 3.1.7"
+
+gem 'active_model_serializers'
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+
+group :development, :test do
+  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,86 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+puts "🌱 Seeding..."
+
+20.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.email
+  phone_number = Faker::PhoneNumber.phone_number_with_country_code
+
+  Client.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    phone_number: phone_number
+  )
+end
+
+employee = Employee.create([
+    {
+        first_name: "David",
+        last_name: "Mutembei",
+        location: "Tala",
+        email: "david@gmail.com"
+    },
+    {
+        first_name: "Morris",
+        last_name: "Obama",
+        location: "Nguluni",
+        email: "morris@gmail.com"
+    },
+    {
+        first_name: "Tom",
+        last_name: "Mutisya",
+        location: "Kathithyamaa",
+        email: "tom@gmail.com"
+    },
+    {
+        first_name: "Ngusya",
+        last_name: "Mule",
+        location: "Kangundo",
+        email: "mule@gmail.com"
+    },
+    {
+        first_name: "Grace",
+        last_name: "Kalondu",
+        location: "Tala",
+        email: "grace@gmail.com"
+    },
+    {
+        first_name: "Rama",
+        last_name: "Scholes",
+        location: "Kangundo",
+        email: "rama@gmail.com"
+    },
+    {
+        first_name: "Moses",
+        last_name: "Mwanzia",
+        location: "Nguluni",
+        email: "mose@gmail.com"
+    },
+])
+
+20.times do
+    date_read = Faker::Date.backward(days: 14)
+    previous_reading = Faker::Number.between(from: 15, to: 100) 
+    current_reading = Faker::Number.between(from: 20, to: 200) 
+    balance = Faker::Number.between(from: 5, to: 1000) 
+    paid = Faker::Boolean.boolean 
+    date_paid = Faker::Date.forward(days: 14)
+  
+    Bill.create(
+        date_read: date_read,
+        previous_reading: previous_reading,
+        current_reading: current_reading,
+        balance: balance,
+        paid: paid,
+        date_paid: date_paid
+    )
+  end
+  
+
+puts "✅ Done seeding!"
