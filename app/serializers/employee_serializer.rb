0f066cad9d4b7b3dca08f0afa524d120832cf26e
Titus Mumo <titusmumo@gmail.com>
2023-06-11 00:43:11 +0300

    editing foreign keys and relationships

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -2,5 +2,5 @@ class EmployeeSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :location
   
   has_many :employee_clients
-  has_many :clients, through: :employee_clients
+  has_many :clients, through: :employee_client
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,109 @@
 require 'faker'
 puts "🌱 Seeding..."
 
+# 20.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   email = Faker::Internet.email
+#   phone_number = Faker::PhoneNumber.phone_number_with_country_code
+#   employee_id = Faker::Number.between(from: 1, to: 7)
+
+#   Client.create(
+#     first_name: first_name,
+#     last_name: last_name,
+#     email: email,
+#     phone_number: phone_number,
+#     employee_id: employee_id
+#   )
+# end
+
+# employee = Employee.create([
+#     {
+#         first_name: "David",
+#         last_name: "Mutembei",
+#         location: "Tala",
+#         email: "david@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+#     {
+#         first_name: "Morris",
+#         last_name: "Obama",
+#         location: "Nguluni",
+#         email: "morris@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+#     {
+#         first_name: "Tom",
+#         last_name: "Mutisya",
+#         location: "Kathithyamaa",
+#         email: "tom@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+#     {
+#         first_name: "Ngusya",
+#         last_name: "Mule",
+#         location: "Kangundo",
+#         email: "mule@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+#     {
+#         first_name: "Grace",
+#         last_name: "Kalondu",
+#         location: "Tala",
+#         email: "grace@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+#     {
+#         first_name: "Rama",
+#         last_name: "Scholes",
+#         location: "Kangundo",
+#         email: "rama@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+#     {
+#         first_name: "Moses",
+#         last_name: "Mwanzia",
+#         location: "Nguluni",
+#         email: "mose@gmail.com",
+#         client_id: balance = Faker::Number.between(from: 1, to: 20)
+#     },
+# ])
+
+CITY_NAMES = ['Tala', 'Nguluni', 'Kathithyamaa']
+# location = CITY_NAMES.sample
+
+
 20.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = Faker::Internet.email
-  phone_number = Faker::PhoneNumber.phone_number_with_country_code
-  employee_id = Faker::Number.between(from: 1, to: 7)
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    email = Faker::Internet.email
+    phone_number = Faker::PhoneNumber.phone_number_with_country_code
+    location = CITY_NAMES.sample
+  
+    employee = Employee.create(
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      location: location,
+    #   client_id: Faker::Number.between(from: 1, to: 20)
+    )
+  
+    client = Client.create(
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      phone_number: phone_number
+    )
+    # employee.update(client_id: client.id)
 
-  Client.create(
-    first_name: first_name,
-    last_name: last_name,
-    email: email,
-    phone_number: phone_number,
-    employee_id: employee_id
-  )
-end
+    employee.update(client_id: client.id)
+    client.update(employee_id: employee.id)
 
-employee = Employee.create([
-    {
-        first_name: "David",
-        last_name: "Mutembei",
-        location: "Tala",
-        email: "david@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-    {
-        first_name: "Morris",
-        last_name: "Obama",
-        location: "Nguluni",
-        email: "morris@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-    {
-        first_name: "Tom",
-        last_name: "Mutisya",
-        location: "Kathithyamaa",
-        email: "tom@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-    {
-        first_name: "Ngusya",
-        last_name: "Mule",
-        location: "Kangundo",
-        email: "mule@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-    {
-        first_name: "Grace",
-        last_name: "Kalondu",
-        location: "Tala",
-        email: "grace@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-    {
-        first_name: "Rama",
-        last_name: "Scholes",
-        location: "Kangundo",
-        email: "rama@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-    {
-        first_name: "Moses",
-        last_name: "Mwanzia",
-        location: "Nguluni",
-        email: "mose@gmail.com",
-        client_id: balance = Faker::Number.between(from: 1, to: 20)
-    },
-])
+    EmployeeClient.create(employee: employee, client: client)
+  end
+  
+
+  
 
 30.times do
     date_read = Faker::Date.backward(days: 14)
